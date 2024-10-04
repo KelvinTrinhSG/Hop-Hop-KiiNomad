4e9a60b45a36cfb4cfc7736169f4a3258467e75a
KelvinTrinhSG <thkien85@Gmail.com>
2024-10-04 10:56:14 +0700

    Xong cho DYM của anh Đạt
    Xong cho DYM của anh Đạt

--- Assets/MummyJumping/Scripts/MintNFTERC721.cs
+++ Assets/MummyJumping/Scripts/MintNFTERC721.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Thirdweb;
@@ -9,8 +9,6 @@ using UnityEngine.SceneManagement;
 public class MintNFTERC721 : MonoBehaviour
 {
     private ThirdwebSDK sdk;
-    public Button mintNFTERC721Btn;
-    public Text mintNFTERC721BtnTxt;
     public Button goBtn;
 
     public Button onChainBtn;
@@ -18,42 +16,17 @@ public class MintNFTERC721 : MonoBehaviour
     public Text scoreTxt;
     public Button replayBtn;
 
-    private string nftContractAddress = "0x05DE6aF60b49bFd552aa7f2fE3011E63f38a9f2a";
-    private string tokenContractAddress = "0x53e713bb63Eb2fb84812e3041ba85aB0AC1A01B4";
+    private string tokenContractAddress = "0x7B46fE9C380228d707fdEf59366dC5006656B251";
 
     void Start()
     {
         sdk = ThirdwebManager.Instance.SDK;
-        mintNFTERC721Btn.gameObject.SetActive(false);
         goBtn.gameObject.SetActive(false);
     }
 
     public void OnConnected() {
-        GetNFTBalance();
+        goBtn.gameObject.SetActive(true);
         GetTokenBalance();
-    }
-
-    public async void GetNFTBalance() {
-        string address = await sdk.Wallet.GetAddress();
-        Contract contract = sdk.GetContract(nftContractAddress);
-        List<NFT> nftList = await contract.ERC721.GetOwned(address);
-        if (nftList.Count > 0)
-        {
-            goBtn.gameObject.SetActive(true);
-            mintNFTERC721Btn.gameObject.SetActive(false);
-        }
-        else {
-            goBtn.gameObject.SetActive(false);
-            mintNFTERC721Btn.gameObject.SetActive(true);
-        }
-    }
-
-    public async void ClaimNFT() {
-        mintNFTERC721Btn.interactable = false;
-        mintNFTERC721BtnTxt.text = "Claiming!";
-        Contract contract = sdk.GetContract(nftContractAddress);
-        var data = await contract.ERC721.Claim(1);
-        GetNFTBalance();
     }
 
     public static int ConvertStringToRoundedInt(string numberStr)
@@ -85,19 +58,31 @@ public class MintNFTERC721 : MonoBehaviour
         sdk = ThirdwebManager.Instance.SDK;
         string address = await sdk.Wallet.GetAddress();
         Contract contract = sdk.GetContract(tokenContractAddress);
-        var tokenBlance = await contract.ERC20.BalanceOf(address);
-        int ownedERC20 = ConvertStringToRoundedInt(tokenBlance.displayValue);
-        int currentScore = GameManager.Ins.Score;
-        if (currentScore > ownedERC20) {
-            int tokenToBeAdded = currentScore - ownedERC20;
-            await contract.ERC20.Claim(tokenToBeAdded.ToString());
-            onChainBtnText.text = "Replay";
-            GetTokenBalance();
+        try
+        {
+            var tokenBlance = await contract.ERC20.BalanceOf(address);
+            int ownedERC20 = ConvertStringToRoundedInt(tokenBlance.displayValue);
+            int currentScore = GameManager.Ins.Score;
+            if (currentScore > ownedERC20)
+            {
+                int tokenToBeAdded = currentScore - ownedERC20;
+                await contract.ERC20.Claim(tokenToBeAdded.ToString());
+                onChainBtnText.text = "Replay";
+                GetTokenBalance();
+            }
+            onChainBtn.interactable = true;
+            onChainBtnText.text = "On Chain";
+            onChainBtn.gameObject.SetActive(false);
+            replayBtn.gameObject.SetActive(true);
         }
-        onChainBtn.interactable = true;
-        onChainBtnText.text = "On Chain";
-        onChainBtn.gameObject.SetActive(false);
-        replayBtn.gameObject.SetActive(true);
+        catch (Exception ex)
+        {
+            Debug.LogError($"Error occurred: {ex.Message}");
+            // Xử lý lỗi, ví dụ hiển thị thông báo cho người dùng hoặc reset lại giao diện
+            onChainBtn.interactable = true;
+            onChainBtnText.text = "On Chain";
+        }
+       
     }
 
 }
--- Assets/MummyJumping/Scripts/WalletConnect.cs
+++ Assets/MummyJumping/Scripts/WalletConnect.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class WalletConnect : MonoBehaviour
 {
     public GameObject GoBtn;
-    public GameObject MintNFTERC721Btn;
 
     // Start is called before the first frame update
     void Start()
     {
         GoBtn.SetActive(false);
-        MintNFTERC721Btn.SetActive(false);
     }
 }
